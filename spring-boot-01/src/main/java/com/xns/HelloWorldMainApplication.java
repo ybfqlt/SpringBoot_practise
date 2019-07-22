5117e41e3a374d2843269c17fe031b61e7df32d0
ltt <823030735@qq.com>
2019-07-22 21:07:12 +0800

    练习1

--- spring-boot-01/src/main/java/com/xns/HelloWorldMainApplication.java
+++ spring-boot-01/src/main/java/com/xns/HelloWorldMainApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 19-7-22 下午4:43
  * @Created by xns
  */
-@SpringBootApplication
+@SpringBootApplication //Spring Boot应用标注在某个类上说明这个类是SpringBoot的主配置类
 public class HelloWorldMainApplication {
     public static void main(String[] args) {
 
