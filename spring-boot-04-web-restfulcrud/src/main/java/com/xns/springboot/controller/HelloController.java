00fa8faa1a4fbc03d8366433ca4d20645705b98e
ltt <823030735@qq.com>
2019-07-24 17:08:29 +0800

    Thymeleaf

--- spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * @Classname HelloController
  * @Description TODO
@@ -17,5 +20,14 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+
+    //查出一些数据，在页面展示
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好!<h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
     }
 }
