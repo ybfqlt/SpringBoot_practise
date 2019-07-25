246e4b457e63fe4bd0df8f801d9cd7cea4a71cf1
ltt <823030735@qq.com>
2019-07-25 16:03:30 +0800

    实现国际化效果

--- spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/config/MyMvcConfig.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/config/MyMvcConfig.java
@@ -1,7 +1,9 @@
 package com.xns.springboot.config;
 
+import com.xns.springboot.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,4 +24,23 @@ public class MyMvcConfig  implements WebMvcConfigurer {
         //浏览器发送 /xns 请求也来到 success 页面
         registry.addViewController("/xns").setViewName("success");
     }
+
+    //所有的WebMvcConfigurer组件都会一起起作用
+    @Bean   //将组件注册在容器
+    public WebMvcConfigurer webMvcConfigurer(){
+        WebMvcConfigurer adapter  = new WebMvcConfigurer(){
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
 }
--- spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/HelloController.java
@@ -16,6 +16,11 @@ import java.util.Map;
 @Controller
 public class HelloController {
 
+    /*@RequestMapping({"/","/index.html"})
+    public String index(){
+        return "index";
+    }*/
+
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
