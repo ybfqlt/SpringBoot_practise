f39c4042f8d5085a5f6a379610894a302e7e4f5c
ltt <823030735@qq.com>
2019-07-26 21:55:04 +0800

    员工修改练习

--- spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -62,6 +64,26 @@ public class EmployeeController {
         //redirect: 重定向到一个地址   /代表当前项目路径
         //forward:转发到一个地址
         //以上两种方法不会经过模版引擎解析拼接
+        return "redirect:/emps";
+    }
+
+    //来到修改页面,查出当前员工,在页面回显
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        //页面要显示所有的部门列表
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        //回到修改页面(add是一个修改添加二合一的页面;
+        return "emp/add";
+    }
+
+    //员工修改,需要提交员工id
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee){
+        System.out.println("修改员工数据:"+employee);
+        employeeDao.save(employee);
         return "redirect:/emps";
     }
 }
