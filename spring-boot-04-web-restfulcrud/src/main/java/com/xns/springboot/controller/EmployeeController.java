1799ae70168dedc9cdc308dafbcc45bb91e85ae7
ltt <823030735@qq.com>
2019-07-27 08:36:36 +0800

    删除员工练习

--- spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/xns/springboot/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.xns.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -84,6 +81,13 @@ public class EmployeeController {
     public String updateEmployee(Employee employee){
         System.out.println("修改员工数据:"+employee);
         employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    //员工删除
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
         return "redirect:/emps";
     }
 }
