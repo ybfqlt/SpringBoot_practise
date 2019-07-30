952d43ef92501e6cca336a09178e9d35bc2d887d
ltt <823030735@qq.com>
2019-07-30 17:12:24 +0800

    practice

--- spring-boot-04-web-jsp/src/main/java/com/xns/springboot/ServletInitializer.java
+++ spring-boot-04-web-jsp/src/main/java/com/xns/springboot/ServletInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+    //重写configure,用configure来告诉一下SpringBoot的主程序在哪,这样应用一启动就知道主程序在哪,然后再来启动主程序
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringBoot04WebJspApplication.class);
