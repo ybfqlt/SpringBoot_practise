8d6ad4303c461b45974a4e4d41b5d8dbf52d160e
ltt <823030735@qq.com>
2019-08-01 19:16:04 +0800

    配置版

--- spring-boot-06-data-mybatis/src/main/java/com/xns/springboot/mapper/EmployeeMapper.java
+++ spring-boot-06-data-mybatis/src/main/java/com/xns/springboot/mapper/EmployeeMapper.java
@@ -8,10 +8,12 @@ package com.xns.springboot.mapper;
  */
 
 import com.xns.springboot.bean.Employee;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 使用@Mapper或者@MapperScan将接口扫描装配到容器中
  */
+//@Mapper
 public interface EmployeeMapper {
 
     public Employee getEmpById(Integer id);
