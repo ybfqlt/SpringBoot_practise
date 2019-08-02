ce98f0d1f65e9c056dc26bf1b7a9c3f9661fbafa
ltt <823030735@qq.com>
2019-08-02 11:20:13 +0800

    练习

---
+++ spring-boot-07/src/main/java/com/xns/springboot/SpringBoot07Application.java
@@ -0,0 +1,13 @@
+package com.xns.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot07Application.class, args);
+	}
+
+}
